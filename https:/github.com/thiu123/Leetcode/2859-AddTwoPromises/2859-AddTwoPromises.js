f9efbb4321cae02410d25dd8c32757dd000167cf
Bùi Trung Hiếu <134358078+thiu123@users.noreply.github.com>
2026-04-15 17:14:29 +0700

    Create 2859-AddTwoPromises

---
+++ https:/github.com/thiu123/Leetcode/2859-AddTwoPromises/2859-AddTwoPromises.js
@@ -0,0 +1,16 @@
+// Last updated: 17:14:32 15/4/2026
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const value1 = await promise1
+    const value2 = await promise2
+    return value1 + value2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
